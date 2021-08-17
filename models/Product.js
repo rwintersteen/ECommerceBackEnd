e7aaac22c85fffb7a78391fdd102b2b33d0e7a3f
Rachael Wintersteen <rachaelwintersteen@gmail.com>
2021-08-16 18:12:49 -0700

    Added foreign keys

--- models/Product.js
+++ models/Product.js
@@ -3,6 +3,9 @@ const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
+// import models
+const Category = require('./Category');
+
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
@@ -10,6 +13,39 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(5,2),
+      allowNull: false,
+      validate:{
+        isDecimal: true,
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate:{
+        isNumeric: true,
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      onDelete: 'SET NULL',
+      references: {
+        model: Category,
+        key: 'id',
+      }
+    }
   },
   {
     sequelize,
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,11 +2,37 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
+// import models
+const Product = require('./Product');
+const Tag = require('./Tag');
+
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      onDelete: 'SET NULL',
+      references: {
+        model: Product,
+        key: 'id',
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      onDelete: 'SET NULL',
+      references: {
+        model: Tag,
+        key: 'id',
+      }
+    },
   },
   {
     sequelize,
@@ -16,5 +42,3 @@ ProductTag.init(
     modelName: 'product_tag',
   }
 );
-
-module.exports = ProductTag;
